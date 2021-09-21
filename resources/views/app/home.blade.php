de45738d382078abb59b4bdf67e63a777fe06987
solangeguzman <solangeguzmanduchen@gmail.com>
2021-09-21 15:24:53 +0200

    prova

---
+++ resources/views/app/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+  <div id="app">
+   EHI CIAO!
+   <example-component>
+     
+   </example-component>
+  
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('posts','PostController');
+
+Route::get('/app', 'WebAppController@home');
